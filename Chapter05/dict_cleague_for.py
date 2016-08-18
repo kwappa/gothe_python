1614f1806e8e5e35ad6f3de42a75c4fc317da84e
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-08-19 00:53:21 +0900

    :baseball: List 5.2

---
+++ Chapter05/dict_cleague_for.py
@@ -0,0 +1,12 @@
+c_league = dict(
+    giants   = '読売ジャイアンツ',
+    tigers   = '阪神タイガース',
+    dragons  = '中日ドラゴンズ',
+    swallows = '東京ヤクルトスワローズ',
+    carp     = '広島東洋カープ',
+    baystars = '横浜DeNAベイスターズ',
+)
+
+for d in c_league:
+    print(d)
+    print(c_league[d])
