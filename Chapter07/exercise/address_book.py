2d63735b32ee057c62727ee9c0e2bd4f50c16755
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-08-20 10:34:24 +0900

    :heavy_plus_sign: add 'Person' class

--- Chapter07/exercise/address_book.py
+++ Chapter07/exercise/address_book.py
@@ -9,3 +9,12 @@ class AddressBook:
 
     def search(self, keyword):  # 検索条件にマッチする登録された人を表示
         pass
+
+class Person:
+    import datetime
+
+    lastname     = ''           # 名前
+    firstname    = ''           # 苗字
+    tel          = ''           # 電話番号
+    mail_address = ''           # メールアドレス
+    birthday     = datetime.datetime(2000, 1, 1) # 生年月日
