8a885b21d33736429a275c0750e3994974ddc3b8
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-08-20 15:41:52 +0900

    :ok_woman: List 8.1

---
+++ Chapter08/test_plus.py
@@ -0,0 +1,11 @@
+import unittest
+
+def plus(a, b):
+    return a + b
+
+class PlusTest(unittest.TestCase):
+    def test_lus(self):
+        self.assertEqual(10, plus(2, 8))
+
+if __name__ == '__main__':
+    unittest.main()
