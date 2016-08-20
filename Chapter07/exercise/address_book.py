9f89d5150bb6813a8444be0d9b090e12d2bddf07
SHIOYA, Hiromu <kwappa.856@gmail.com>
2016-08-20 10:31:17 +0900

    :muscle: Start execise of chapter 07
    * create blank class of address_book

---
+++ Chapter07/exercise/address_book.py
@@ -0,0 +1,11 @@
+class AddressBook:
+    person_list = []            # 登録された人の一覧
+
+    def add(self, person):      # アドレス帳に人を追加
+        pass
+
+    def show_all(self):         # 登録された人の一覧を表示
+        pass
+
+    def search(self, keyword):  # 検索条件にマッチする登録された人を表示
+        pass
